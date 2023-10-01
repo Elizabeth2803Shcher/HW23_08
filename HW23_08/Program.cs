bb661c51d530c3bcfbba680ed30d992115382d5d
Elizabeth2803 <elizaart2801@gmail.com>
2023-10-01 21:13:57 +0200

    Добавьте файлы проекта.

---
+++ HW23_08/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Two roads diverged in a yellow wood,");
+Console.WriteLine("And sorry I could not travel both");
+Console.WriteLine("And be one traveler, long I stood");
+Console.WriteLine("And looked down one as far as I could");
+Console.WriteLine("To where it bent in the undergrowth.");
+Console.WriteLine("                                         ");
+//2
+Console.WriteLine("Вторая задача: ");
+Console.WriteLine($" {1} {13} {49}");
+Console.WriteLine("                                         ");
+//3
+Console.WriteLine("Третья задача: ");
+Console.Write("Введите 1-е число: ");
+double n = double.Parse(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+double m = double.Parse(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine($" {n}\t{m}\t{y}");
